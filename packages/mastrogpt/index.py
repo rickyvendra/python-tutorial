1a5a91b46b5fbc9480335ad924fcab0080efa2dd
rickyvendra <83713492+rickyvendra@users.noreply.github.com>
2024-10-10 16:31:11 +0100

    test

--- packages/mastrogpt/index.py
+++ packages/mastrogpt/index.py
@@ -5,6 +5,10 @@ def main(arg):
     data = {
         "services": [
             ## 1c add the demo entry, an object with name Demo and url mastrogpt/demo
+            {
+                "name": "Demo",
+                "url": "mastrogpt/demo"
+            }
             
             ## 2b add the OpenAI entry, an object with name Demo and url mastrogpt/demo
         ]
